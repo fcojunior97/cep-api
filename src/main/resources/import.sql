cecdc5624ba428e1eec22f457423ad11f57a0778
Francisco Junior <jrpires.git@gmail.com>
2024-12-26 23:25:28 -0400

    adicionando novos dados de teste

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,9 @@
 INSERT INTO cep (cep, logradouro, bairro, cidade, estado, uf, regiao, ddd) VALUES ('01000000', 'Praça da Sé', 'Sé', 'São Paulo', 'São Paulo', 'SP', 'Sudeste', '11');
 INSERT INTO cep (cep, logradouro, bairro, cidade, estado, uf, regiao, ddd) VALUES ('03022080', 'Rua Batista Parente', 'Alto do Pari', 'São Paulo', 'São Paulo', 'SP', 'Sudeste', '11');
+INSERT INTO cep (cep, logradouro, bairro, cidade, estado, uf, regiao, ddd) VALUES ('04156080', 'Travessa Bananeiras', 'Água Funda', 'São Paulo', 'São Paulo', 'SP', 'Sudeste', '11');
+INSERT INTO cep (cep, logradouro, bairro, cidade, estado, uf, regiao, ddd) VALUES ('04156055', 'Travessa Engenheiro Mário Trindade', 'Água Funda', 'São Paulo', 'São Paulo', 'SP', 'Sudeste', '11');
 INSERT INTO cep (cep, logradouro, bairro, cidade, estado, uf, regiao, ddd) VALUES ('13474534', 'Rua Aderaldo Ferreira de Araújo', 'Antônio Zanaga', 'Americana', 'São Paulo', 'SP', 'Sudeste', '11');
 INSERT INTO cep (cep, logradouro, bairro, cidade, estado, uf, regiao, ddd) VALUES ('63900235', 'Rua Aderaldo Ferreira de Araújo', 'Centro', 'Quixadá', 'Ceará', 'CE', 'Nordeste', '88');
 INSERT INTO cep (cep, logradouro, bairro, cidade, estado, uf, regiao, ddd) VALUES ('69650970', 'Rua Augusto Luzeiro', 'Centro', 'Atalaia do Norte', 'Amazonas', 'AM', 'Norte', '92');
+
+INSERT INTO usuario (login, senha) VALUES ('fcojunior', '$2a$12$ejKGUQoYMi6MOy2sS8..o.moE6TFLvcOXLAF.Qp6wbS7zfvW10yzK')
